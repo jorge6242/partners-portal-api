77b9e7554459ce49e9b91428dea5dc371bebe78b
Jorge Gomez <jorge6242@gmail.com>
2020-05-11 17:06:00 -0400

    update changes

--- config/filesystems.php
+++ config/filesystems.php
@@ -70,17 +70,6 @@ return [
         'reportepagos' => [
             'driver' => 'local',
             'root' => public_path('images/reportepagos'),
-            'visibility' => 'public',
-            'permissions' => [
-                'file' => [
-                    'public' => 0664,
-                    'private' => 0600,
-                ],
-                'dir' => [
-                    'public' => 0775,
-                    'private' => 0700,
-                ],
-            ],
         ],
     ],
 
