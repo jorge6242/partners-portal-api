6caf1fc564ad285c4fdd2b8016a633a98998e114
Jorge <jorge6242@gmail.com>
2020-05-22 01:39:38 -0400

    update cascade delete

--- app/Repositories/WidgetRepository.php
+++ app/Repositories/WidgetRepository.php
@@ -81,9 +81,11 @@ class WidgetRepository  {
     }
 
     public function delete($id) {
-     return $this->model->find($id)->delete();
+      $data = $this->model->find($id);
+      $data->widgetRole()->delete();
+      $data->delete();
+     return $data;
     }
-
     public function checkRecord($name)
     {
       $data = $this->model->where('description', $name)->first();
